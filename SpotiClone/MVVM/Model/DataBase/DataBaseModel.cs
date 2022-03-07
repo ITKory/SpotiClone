5ead1a9ea727dad7503507f24bf4c98be664b5c0
Alex Kor <Skoryakin3@gmail.com>
2022-03-07 16:39:46 +0300

    Добавьте файлы проекта.

---
+++ SpotiClone/MVVM/Model/DataBase/DataBaseModel.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotiClone.MVVM.Model.DataBase
+{
+    public class DataBaseModel
+    {
+        private RestClient _client;
+        private RestRequest _request;
+
+        public DataBaseModel()
+        {
+            _client = new();
+            _client.Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator("token", "Bearer");
+
+        }
+
+         public void SendRequest( string path)
+        {
+            _request = new RestRequest(path, Method.Get);
+            _request.AddHeader("Accept", "aplication/json");
+            _request.AddHeader("Content-Type", "aplication/json");
+        }
+
+        public T GetResponse<T>()
+        {
+            var response = _client.GetAsync(_request).Result;
+            var data = JsonConvert.DeserializeObject<T>(response.Content);
+            if (data is null)
+                throw new NullReferenceException($"track list is null; response status code:{response.StatusCode}");
+            return data;
+        }
+
+ 
+
+    }
+
+}
---
+++ SpotiClone/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using SpotiClone.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using RestSharp.Authenticators.OAuth2;
+using Newtonsoft.Json;
+using System.Windows.Controls;
+using SpotiClone.MVVM.Model.DataBase;
+
+namespace SpotiClone.MVVM.ViewModel
+{
+    internal class MainViewModel
+    {
+        public List<Track> Songs { get; set; }
+        public ArtistModel ArtistInfo { get; set; }
+ 
+        private DataBaseModel _context;
+
+        private Track _selectedTrack;
+        public Track SelectedTrack
+        {
+            get => _selectedTrack; 
+            set
+            {
+                _selectedTrack = value;
+            }
+        }
+        public MainViewModel()
+        {
+            
+
+            _context = new();
+          
+            _context.SendRequest("https://api.spotify.com/v1/artists/7dH8w9flSy9w81ilr0xXWe/top-tracks?market=ES");
+            Songs = _context.GetResponse<TrackModel>().Tracks.ToList();
+
+            _context.SendRequest("https://api.spotify.com/v1/artists/7dH8w9flSy9w81ilr0xXWe");
+            ArtistInfo = _context.GetResponse<ArtistModel>();
+
+
+        }
+
+    
+    }
+}
